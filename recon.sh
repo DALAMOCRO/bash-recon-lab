abf23a77d72be91996c047e018b8524569c49226
DALAMOCRO <orco497@gmail.com>
2026-05-15 13:10:12 +0100

    Add files via upload

---
+++ recon.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "====BASIC RECON===="
+
+echo ""
+echo "Current User"
+whoami
+
+echo ""
+echo "Current Directory"
+pwd
+
+echo ""
+echo "Hostname :"
+hostname
+
+echo ""
+echo "Network Information :"
+ipconfig
+
+
